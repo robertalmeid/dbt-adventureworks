941d08e307c13ff78a5e3ba01de5195795d74f80
Roberto Almeida <robert.almeid@hotmail.com>
2024-08-17 13:06:39 +0000

    add campo stateprovindeid na tabela enderecos

--- models/staging/stg_erp__enderecos.sql
+++ models/staging/stg_erp__enderecos.sql
@@ -3,8 +3,10 @@ with
         select 
         cast(ADDRESSID as int) as pk_enderecoid
         , cast(ADDRESSLINE1 as string) as endereco
+        , cast(STATEPROVINCEID as int) as fk_cidadeid
         , cast(CITY as string) as cidade
         , cast(STATEPROVINCEID as int) fk_estadoid
+        , *
         from {{ source('erp', 'address') }}
     )
     select 
